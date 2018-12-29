f0caddc9285264e785556908ad0d1beccbfd1232
Dmitry <deloakk.mr@mail.ru>
2018-12-29 12:54:50 +0300

    Занес создание массива картинок в функцию

--- js/elements-generator.js
+++ js/elements-generator.js
@@ -6,21 +6,29 @@
     
     var pictureTemplate = document.querySelector('.picture-template').content.querySelector('.picture');
     
-    var pictures = [];
     
-    for (var picNum = 1; picNum < 26; picNum++) {
-        pictures[i] = {
-            url: `photos/${picNum}.jpg`,
-            likes: window.randomizer.GenerateRandomNumber(likesCountMin, likesCountMax),
-            comments: window.data.GetRandomComments()
-        };
-    }
+    
     
     window.elementsGenerator = {
-        GeneratePicture: function (picture) {
+        picturesCount: picturesCount,
+        CreatePictures: function () {
+            var pictures = [];
+            
+            for (var picNum = 1; picNum < 26; picNum++) {
+                pictures[i] = {
+                    url: `photos/${picNum}.jpg`,
+                    likes: window.randomizer.GenerateRandomNumber(likesCountMin, likesCountMax),
+                    comments: window.data.GetRandomComments()
+                };
+            }
+            
+            return pictures;
+        },
+        
+        GeneratePicture: function () {
             var nodePicture = pictureTemplate.cloneNode(true);
             
-            nodePicture.querySelector('.picture-img').setAttribute('src', `${picture.url}`);
+            nodePicture.querySelector('.picture-img').setAttribute('src', `${pictures.url}`);
             nodePicture.querySelector('.picture-likes').textContent = `${picture.likes}`;
             
             picture.comments.forEach(function (comment) {
@@ -30,4 +38,4 @@
             return nodePicture;
         }
     };
-});
+})();
