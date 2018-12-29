466d74785196041115c20afce8bd74dafa486f15
Dmitry <deloakk.mr@mail.ru>
2018-12-29 13:53:06 +0300

    Дописал показ картинки в отдельном окне

--- js/pictures.js
+++ js/pictures.js
@@ -5,14 +5,20 @@
     var galleryOverlay = document.querySelector('.gallery-overlay');
     var galleryOverlayPreview = galleryOverlay.querySelector('.gallery-overlay-preview');
     var galleryOverlayImg = galleryOverlayPreview.querySelector('.gallery-overlay-image');
-    var galleryOverlayImgLikes = galleryOverlayImg.querySelector('.gallery-overlay-controls-like');
-    var galleryOverlayImgComments = galleryOverlayImg.querySelector('.gallery-overlay-controls-comments');
+    var galleryOverlayLikes = galleryOverlayPreview.querySelector('.gallery-overlay-controls-like').querySelector('.likes-count');
+    var galleryOverlayComments = galleryOverlayPreview.querySelector('.gallery-overlay-controls-comments');
     
     var picturesFragment = document.createDocumentFragment();
     for (var picNum = 0; picNum < window.elementsGenerator.picturesCount; picNum++) {
         picturesFragment.appendChild(window.elementsGenerator.GeneratePicture(pictures[picNum]));
     }
     picturesContainer.appendChild(picturesFragment);
+    
+    var picturesDom = picturesContainer.querySelectorAll('.picture');
+    
+    galleryOverlayImg.src = `${picturesDom[0].querySelector('.picture-img').getAttribute('src')}`;
+    galleryOverlayLikes.textContent = `${picturesDom[0].querySelector('.picture-likes').textContent}`;
+    galleryOverlayComments.querySelector('.comments-count').textContent = `${picturesDom[0].querySelector('.picture-comments').textContent}`;
     
     galleryOverlay.classList.remove('hidden');
 })();
