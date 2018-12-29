f107443500bd8a412ae6b5ceb1194b3a4dd8ac6a
Dmitry <deloakk.mr@mail.ru>
2018-12-29 12:41:49 +0300

    Дописал функцию генерации картинки

--- js/elements-generator.js
+++ js/elements-generator.js
@@ -4,13 +4,30 @@
     const likesCountMin = 15;
     const likesCountMax = 200;
     
+    var pictureTemplate = document.querySelector('.picture-template').content.querySelector('.picture');
+    
     var pictures = [];
     
     for (var picNum = 1; picNum < 26; picNum++) {
         pictures[i] = {
             url: `photos/${picNum}.jpg`,
             likes: window.randomizer.GenerateRandomNumber(likesCountMin, likesCountMax),
-            comments: window.data.GetRandomComments();
+            comments: window.data.GetRandomComments()
         };
     }
+    
+    window.elementsGenerator = {
+        GeneratePicture: function (picture) {
+            var nodePicture = pictureTemplate.cloneNode(true);
+            
+            nodePicture.querySelector('.picture-img').setAttribute('src', `${picture.url}`);
+            nodePicture.querySelector('.picture-likes').textContent = `${picture.likes}`;
+            
+            picture.comments.forEach(function (comment) {
+                nodePicture.querySelector('.picture-comments').textContent += comment;
+            });
+            
+            return nodePicture;
+        }
+    };
 });
