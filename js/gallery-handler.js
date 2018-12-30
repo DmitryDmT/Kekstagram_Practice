db1f76da6baad995de0190f8d9a8761e1a502dbc
DmitryDmD <deloakk.mr@mail.ru>
2018-12-31 01:37:23 +0300

    Начал писать обработчики open/close по нажатию на клавиши

--- js/gallery-handler.js
+++ js/gallery-handler.js
@@ -7,21 +7,65 @@
   var galOverlayLikes = galOverlay.querySelector('.likes-count');
   var galOverlayComments = galOverlay.querySelector('.comments-count');
   
-  picturesContainer.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    if (evt.target.classList.contains('picture-img')) {
-      var picture = evt.target.parentElement;
-      
-      galOverlayImg.src = `${picture.querySelector('.picture-img').src}`;
-      galOverlayLikes.textContent = `${picture.querySelector('.picture-likes').textContent}`;
-      galOverlayComments.textContent = `${picture.querySelector('.picture-comments').textContent}`;
-      galOverlay.classList.remove('hidden');
-    }
-  });
+  var FillOverlayData = function (picture) {
+    galOverlayImg.src = `${picture.querySelector('.picture-img').src}`;
+    galOverlayLikes.textContent = `${picture.querySelector('.picture-likes').textContent}`;
+    galOverlayComments.textContent = `${picture.querySelector('.picture-comments').textContent}`;
+  };
   
-  galOverlayCloseButton.addEventListener('click', function (evt) {
+  var ShowPicture = function (evt) {
+    evt.preventDefault();
+    var picture;
+    
+    
+    if (evt.target.classList.contains('picture-img')) {
+      picture = evt.target.parentElement;
+    }
+    else {
+      picture = evt.target; // TODO доработать функцию
+    }
+    
+      
+    FillOverlayData(picture);
+      
+    galOverlay.classList.remove('hidden');
+      
+    document.addEventListener('keydown', OnPictureEscClose);
+    galOverlayCloseButton.addEventListener('keydown', OnPictureEnterClose);
+    galOverlayCloseButton.addEventListener('click', OnPictureClickClose);
+  };
+  
+  var OnPictureClickClose = function (evt) {
     evt.preventDefault();
     
     galOverlay.classList.add('hidden');
+    document.removeEventListener('keydown', OnPictureEscClose);
+  };
+  
+  var OnPictureEnterClose = function (evt) {
+    evt.preventDefault();
+    
+    if (evt.keyCode === 13) {
+      galOverlay.classList.add('hidden');
+      document.removeEventListener('keydown', OnPictureEscClose);
+    }
+  };
+  
+  var OnPictureEscClose = function (evt) {
+    if (evt.keyCode === 27) {
+      galOverlay.classList.add('hidden');
+      document.removeEventListener('keydown', OnPictureEscClose);
+    }
+  };
+  
+  picturesContainer.addEventListener('click', function (evt) {
+    ShowPicture(evt);
+  });
+  
+  picturesContainer.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 13) {
+      console.log(evt.target);
+      ShowPicture(evt);
+    }
   });
 })();
