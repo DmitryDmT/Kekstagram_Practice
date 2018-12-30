b7f98e3cfb539447047f87f0b291467644512d06
DmitryDmD <deloakk.mr@mail.ru>
2018-12-30 23:39:26 +0300

    Подключил модуль обработчика галереи

---
+++ js/gallery-handler.js
@@ -0,0 +1,5 @@
+(function () {
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+  var galleryOverlayCloseButton = galleryOverlay.querySelector('.gallery-overlay-close');
+  
+})();
