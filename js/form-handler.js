ac88cbad79bb4cba382fe1ab5c06827e8068cab4
DmitryDmD <deloakk.mr@mail.ru>
2018-12-31 12:55:40 +0300

    Добавил open/close для формы загрузки картинок

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,3 +1,15 @@
 (function () {
+  var formUpload = document.querySelector('.upload-form');
+  var fileUploadField = formUpload.querySelector('#upload-file');
   
+  var overlayUploadContainer = formUpload.querySelector('.upload-overlay');
+  var formCancelButton = formUpload.querySelector('.upload-form-cancel');
+  
+  fileUploadField.addEventListener('change', function () {
+    overlayUploadContainer.classList.remove('hidden');
+    
+    formCancelButton.addEventListener('click', function () {
+      overlayUploadContainer.classList.add('hidden');
+    });
+  });
 })();
