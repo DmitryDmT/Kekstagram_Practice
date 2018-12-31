cdd31c01ba501a818f6a4848c1b7abf9c5e2b06b
DmitryDmD <deloakk.mr@mail.ru>
2018-12-31 22:46:22 +0300

    Закончил обработчик open/close формы загрузки изображения

--- js/form-handler.js
+++ js/form-handler.js
@@ -5,11 +5,22 @@
   var overlayUploadContainer = formUpload.querySelector('.upload-overlay');
   var formCancelButton = formUpload.querySelector('.upload-form-cancel');
   
+  var OnEscCancelForm = function (evt) {
+    if (evt.keyCode === 27) {
+      overlayUploadContainer.classList.add('hidden');
+      document.removeEventListener('keydown', OnEscCancelForm);
+    }
+  };
+  
+  var OnClickCancelForm = function () {
+    overlayUploadContainer.classList.add('hidden');
+    formCancelButton.removeEventListener('keydown', OnEscCancelForm);
+  };
+  
   fileUploadField.addEventListener('change', function () {
     overlayUploadContainer.classList.remove('hidden');
     
-    formCancelButton.addEventListener('click', function () {
-      overlayUploadContainer.classList.add('hidden');
-    });
+    formCancelButton.addEventListener('click', OnClickCancelForm);
+    document.addEventListener('keydown', OnEscCancelForm);
   });
 })();
