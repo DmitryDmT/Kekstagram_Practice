1b24af32510e31594c6126d14e3599fb7834bd2f
DmitryDmD <deloakk.mr@mail.ru>
2019-01-04 17:01:43 +0300

    Добавил возможность изменения масштаба изображения

--- js/form-handler.js
+++ js/form-handler.js
@@ -4,20 +4,49 @@
   
   var overlayUploadContainer = formUpload.querySelector('.upload-overlay');
   var formCancelButton = formUpload.querySelector('.upload-form-cancel');
-  
   var formUploadDescription = formUpload.querySelector('.upload-form-description');
   
   var formUploadResizeControls = formUpload.querySelector('.upload-resize-controls-value');
+  var formUploadResizeDecrement = formUpload.querySelector('.upload-resize-controls-button-dec');
+  var formUploadResizeIncrement = formUpload.querySelector('.upload-resize-controls-button-inc');
+  const resizeStep = 25;
+  const resizeMax = 100;
+  const resizeMin = 25;
+  
   var formUploadEffectControls = formUpload.querySelector('.upload-effect-controls');
   var formUploadImage = formUpload.querySelector('.effect-image-preview');
   
-  formUploadEffectControls.addEventListener('change', function (evt) {
-    var value = evt.target.value;
-
-    formUploadImage.className = `effect-image-preview effect-${value}`;
+  /* Setup form picture */
+  
+  formUploadResizeIncrement.addEventListener('click', function () {
+    var resizeUpValue = parseInt(formUploadResizeControls.getAttribute('value'));
+    if (resizeUpValue < resizeMax) {
+      var upValue = resizeUpValue + resizeStep;
+      formUploadResizeControls.setAttribute('value', `${upValue}%`);
+      if (upValue < 100) {
+        formUploadImage.style.transform = `scale(0.${upValue})`;
+      }
+      else {
+        formUploadImage.style.transform = `scale(${upValue / 100})`;
+      }
+    }
   });
   
+  formUploadResizeDecrement.addEventListener('click', function () {
+    var resizeDownValue = parseInt(formUploadResizeControls.getAttribute('value'));
+    if (resizeDownValue > resizeMin) {
+      var downValue = resizeDownValue - resizeStep;
+      formUploadResizeControls.setAttribute('value', `${downValue}%`);
+      formUploadImage.style.transform = `scale(0.${downValue})`;
+    }
+  });
   
+  formUploadEffectControls.addEventListener('change', function (evt) {
+    var effectValue = evt.target.value;
+    formUploadImage.className = `effect-image-preview effect-${effectValue}`;
+  });
+  
+  /* Open/Close form picture */
   
   var CloseForm = function () {
     overlayUploadContainer.classList.add('hidden');
