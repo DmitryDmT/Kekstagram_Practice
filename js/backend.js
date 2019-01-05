68846952cf32346cfab518e3c8e73ea8c928a8a2
DmitryDmD <deloakk.mr@mail.ru>
2019-01-06 00:41:38 +0300

    Дописал модуль взаимодействия с сервером backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,3 +1,67 @@
 (function () {
+  const URL = 'https://js.dump.academy/kekstagram';
+  const SUCCESS_STATUS = 200;
+  const TIMEOUT = 10000;
   
+  window.backend = {
+    download: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      
+      xhr.responeType = 'json';
+      
+      xhr.addEventListener('load', function () {
+        switch (xhr.status) {
+          case SUCCESS_STATUS:
+            onLoad(xhr.response);
+            break;
+          default:
+            onError(`Неизвестная ошибка: ${xhr.status}, ${xhr.statusText}`);
+            break;
+        }
+      });
+      
+      xhr.addEventListener('error', function () {
+        onError(`Ошибка соединения`);
+      });
+      
+      xhr.addEventListener('timeout', function () {
+        onError(`Превышено время ожидания ответа от сервера`);
+      });
+      
+      xhr.timeout = TIMEOUT;
+      
+      xhr.open('GET', `${URL}/data`);
+      xhr.send();
+    },
+    
+    upload: function (formData, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      
+      xhr.responseType = 'json';
+      
+      xhr.addEventListener('load', function () {
+        switch (xhr.status) {
+          case SUCCESS_STATUS:
+            onLoad(xhr.response);
+            break;
+          default:
+            onError('Превышено время ожидания ответа от сервера');
+            break;
+        }
+      });
+      
+      xhr.addEventListener('error', function () {
+        onError('Ошибка соединения');
+      });
+      
+      xhr.addEventListener('timeout', function () {
+        onError('Превышено время ожидания ответа от сервера');
+      });
+      
+      xhr.timeout = TIMEOUT;
+      
+      xhr.open('POST', URL);
+      xhr.send(formData);
+    }
+  };
 })();
